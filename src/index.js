83d351ffa74d1e7786ed998ed89df61ee583198e
VLeshkevich <leshkevich.veronica@gmail.com>
2018-03-05 17:52:41 +0300

    feature/zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zeros = 0;
+
+  while(number >= 5){
+  	number = Math.floor(number/5);
+  	zeros = zeros + number;
+  }
+  
+  return zeros;
 }
